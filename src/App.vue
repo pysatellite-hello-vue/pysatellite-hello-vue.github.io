d9882316a8749ea196dd8b23cb6385f16f9fd43f
pysatellite <pysatellite@gmail.com>
2024-01-31 12:03:51 +0900

    hello world

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 
-// reactive state
-const count = ref({
-	cnt: 0,
-	name: "KIM",
-	
-})
-
-// functions that mutate state and trigger updates
-function increment() {
-  count.value.cnt = count.value.cnt + 1
-  count.value.name = count.value.name.split('').reverse().join('')
-}
-
-// lifecycle hooks
-onMounted(() => console.log(`The initial count is ${count.value}.`))
+const message = ref('Hello World!')
 </script>
 
 <template>
-  <button @click="increment">Count is: {{ count.cnt }} {{ count.name }}</button>
+  <h1>{{ message }}</h1>
 </template>
-
