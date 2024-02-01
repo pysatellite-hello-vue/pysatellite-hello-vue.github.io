5f711f283f330e1e5e1033e3d101884af7470240
dmario24 <becky2sawyer@gmail.com>
2024-02-02 07:42:27 +0900

    a

---
+++ src/a.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+let id = 0
+
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+])
+
+function addTodo() {
+  // ...
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  // ...
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+</template>
