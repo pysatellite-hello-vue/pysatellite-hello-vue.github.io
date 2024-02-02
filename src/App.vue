debc8f89741b77d922102dd9eb5b79e2fadee3c7
pysatellite <pysatellite@gmail.com>
2024-02-02 11:15:57 +0900

    아침체조

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
 import { ref } from 'vue'
-import Child from './components/Child.vue'
 
-const pocketMoney = ref('퇴근하셔도 좋아요. 수고해셨습니다 : D')
+const show = ref(true)
+const list = ref([1, 2, 3])
 </script>
 
 <template>
-  <Child :pocketMoney="pocketMoney" />
+ <button @click="show = !show">Toggle List</button>
+ <button @click="list.push(list.length + 1)">Push Number</button>
+ <button @click="list.pop()">Pop Number</button>
+ <button @click="list.reverse()">Reverse List</button>
+
+ <ul v-if="show && list.length">
+   <li v-for="item of list">{{ item }}</li>
+ </ul>
+ <p v-else-if="list.length">List is not empty, but hidden.</p>
+ <p v-else>List is empty.</p>
 </template>
+
